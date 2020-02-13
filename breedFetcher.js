3e210bff6f397f8cb0d4e7b7febb9e92ce3597ad
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-13 21:22:34 +0000

    implemented index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,36 +1,33 @@
 const request = require('request');
 
-let input = process.argv.slice(2);
 
 
 
-let string = input[0].slice(0, 3);
-//console.log(string);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${string}`, (error, response, body) => {
-  // console.error('error', error);
-  // console.log('status', response && response.statusCode);
-  if (!error) {
-    const data = JSON.parse(body);
-    //console.log(data);
-    //console.log(typeof data);
-    let data2 = null;
-    if (data.length > 1) {
-      for (let item in data) {
-        //console.log(data[item]);
-        if (data[item]['name'] === input[0]) {
-          data2 = data[item];
+const fetchBreedDescription = (breedName, callback) => {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName.slice(0, 3)}`, (error, response, body) => {
+    if (!error) {
+      const data = JSON.parse(body);
+      let data2 = null;
+      if (data.length > 1) {
+        for (let item in data) {
+          if (data[item]['name'] === breedName) {
+            data2 = data[item];
+          }
         }
       }
-    }
-    if (data2 === null) {
-      console.log('NO RESULT FOUND');
+      if (data2 !== null) {
+        callback(null, data2['description']);
+      } else {
+        callback(error);
+      }
     } else {
-      console.log(data2['description']);
+      callback(error);
     }
-  } else {
-    console.error('ERROR', error);
-    console.log('STATUS', response && response.statusCode);
-  }
-  
-});
+    
+  });
+
+};
+
+module.exports = {fetchBreedDescription};
---
+++ index.js
@@ -0,0 +1,14 @@
+const {fetchBreedDescription} = require('./breedFetcher');
+
+
+let input = process.argv.slice(2);
+let string = input[0];
+
+fetchBreedDescription(string, (error, desc) => {
+  
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(desc);
+  }
+});
